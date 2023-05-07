09201edc150c9a4dbf3ffeb4d5ae9aea4b7f4eb9
moviefan322 <philman202@yahoo.com>
2023-05-07 13:16:28 -0400

    search function works

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -14,19 +14,23 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, intialState)
 
-
-  const fetchUsers = async () => {
+  const searchUsers = async (text) => {
     setLoading()
-    const res = await fetch(`${GITHUB_URL}/users`, {
+
+    const params = new URLSearchParams({
+      q: text,
+    })
+
+    const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     })
-    const data = await res.json()
+    const { items } = await res.json()
 
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      payload: items,
     })
   }
 
@@ -34,7 +38,7 @@ export const GithubProvider = ({ children }) => {
 
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{ users: state.users, loading: state.loading, searchUsers }}
     >
       {children}
     </GithubContext.Provider>
