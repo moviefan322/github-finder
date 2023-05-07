5122227fb37dc24e1c3d3e96c8939b4bd4d32634
moviefan322 <philman202@yahoo.com>
2023-05-07 13:12:43 -0400

    adds search form

--- src/App.js
+++ src/App.js
@@ -17,13 +17,14 @@ function App() {
         <div className='flex flex-col justify-between h-screen'>
           <Navbar />
 
-          <main className='container mx-auto px-3 pb-12'>Content</main>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='*' element={<NotFound />} />
-            <Route path='/notfound' element={<NotFound />} />
-          </Routes>
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='*' element={<NotFound />} />
+              <Route path='/notfound' element={<NotFound />} />
+            </Routes>
+          </main>
           <Footer />
         </div>
       </Router>
