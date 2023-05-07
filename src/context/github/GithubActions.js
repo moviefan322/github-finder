27b9995ae820c4a88949d644389426c9a176f53a
moviefan322 <philman202@yahoo.com>
2023-05-07 16:14:19 -0400

    adds Actions context and dispatches getUsers from component

---
+++ src/context/github/GithubActions.js
@@ -0,0 +1,21 @@
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+export const searchUsers = async (text) => {
+  const params = new URLSearchParams({
+    q: text,
+  })
+
+  const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`,
+    },
+  })
+  const { items } = await res.json()
+
+  return items
+}
+
+module.exports = {
+  searchUsers,
+}
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -16,26 +16,6 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, intialState)
 
-  const searchUsers = async (text) => {
-    setLoading()
-
-    const params = new URLSearchParams({
-      q: text,
-    })
-
-    const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    })
-    const { items } = await res.json()
-
-    dispatch({
-      type: 'GET_USERS',
-      payload: items,
-    })
-  }
-
   const getUser = async (login) => {
     console.log('poop', login)
     setLoading()
@@ -86,11 +66,8 @@ export const GithubProvider = ({ children }) => {
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        user: state.user,
-        loading: state.loading,
-        repos: state.repos,
-        searchUsers,
+        ...state,
+        dispatch,
         clearUsers,
         getUser,
         getUserRepos,
