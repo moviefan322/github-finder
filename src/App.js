22ba2abfcb083aa0c6e50a88cf7c42eaba110306
moviefan322 <philman202@yahoo.com>
2023-05-07 12:51:41 -0400

    instantiates global context for userfetch and storage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import { GithubProvider } from './context/github/GithubContext'
 
 function App() {
+  if (typeof window !== 'undefined') {
+    window.React = React
+  }
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar />
 
-        <main className='container mx-auto px-3 pb-12'>Content</main>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='*' element={<NotFound />} />
-          <Route path='/notfound' element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+          <main className='container mx-auto px-3 pb-12'>Content</main>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<NotFound />} />
+            <Route path='/notfound' element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   )
 }
 
